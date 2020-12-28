2dcda08917fcbd8e806201bfae630e84d1ecdfb8
joshuasearle <joshryansearle@gmail.com>
2020-12-28 22:38:18 +1100

    Add util func

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,19 @@
+import { time } from './types';
+
 export const capitalise = (s: string): string => {
   return s.charAt(0).toUpperCase() + s.slice(1);
+};
+
+export const timeObjToString = ({ hours, minutes }: time): string => {
+  const hoursString = String(hours);
+  const minutesString = String(minutes);
+  const doubleHoursString =
+    hoursString.length === 2 ? hoursString : '0' + hoursString;
+  const doubleMinutesString =
+    minutesString.length === 2 ? minutesString : '0' + minutesString;
+  return doubleHoursString + ':' + doubleMinutesString;
+};
+
+export const timeToHours = ({ hours, minutes }: time): number => {
+  return;
 };
