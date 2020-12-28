5d247568b629de18d40d7e2f8781895c6af2fcc2
joshuasearle <joshryansearle@gmail.com>
2020-12-28 23:35:22 +1100

    Add widmark formula

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,4 @@
-import { time } from './types';
+import { time, gender } from './types';
 
 export const capitalise = (s: string): string => {
   return s.charAt(0).toUpperCase() + s.slice(1);
@@ -16,4 +16,29 @@ export const timeObjToString = ({ hours, minutes }: time): string => {
 
 export const timeToHours = ({ hours, minutes }: time): number => {
   return;
+};
+
+export const widmark = (
+  drinks: number,
+  standardDrink: number,
+  bodyWeight: number,
+  gender: gender,
+  timeElapsed: number
+): number => {
+  const waterToTotalWeightRatio =
+    gender === 'male' ? 0.68 : gender === 'female' ? 0.55 : 0.615;
+
+  const bodyWeightInGrams = bodyWeight * 1000;
+  const waterBodyWeight = bodyWeightInGrams * waterToTotalWeightRatio;
+
+  const alcoholInGrams = drinks * standardDrink;
+
+  const alcoholUnitPerWaterWeightUnit = alcoholInGrams / waterBodyWeight;
+  const densityOfBlood = 1.055;
+  const bacBeforeMetabolisation =
+    alcoholUnitPerWaterWeightUnit * densityOfBlood * 100;
+
+  const alcoholMetabolised = 0.015 * timeElapsed;
+  const totalBac = bacBeforeMetabolisation - alcoholMetabolised;
+  return totalBac;
 };
