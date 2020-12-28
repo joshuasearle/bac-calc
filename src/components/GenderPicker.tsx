1bdfdc46865a5497b29f2578fb67d038d77b9e60
joshuasearle <joshryansearle@gmail.com>
2020-12-28 22:22:11 +1100

    Create component for picking a gender

---
+++ src/components/GenderPicker.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { gender } from '../types';
+import { capitalise } from '../util';
+
+import GenderRadio from './GenderRadio';
+
+interface GenderPickerProps {
+  currentGender: gender;
+  genderChangeHandler: any;
+}
+
+const genders: gender[] = ['male', 'female', 'other'];
+
+const GenderPicker: React.FC<GenderPickerProps> = ({
+  currentGender,
+  genderChangeHandler,
+}: GenderPickerProps) => {
+  // List of genders as radio buttons
+  const genderRadios = genders.map((g: gender) => (
+    <GenderRadio
+      currentGender={currentGender}
+      gender={g}
+      label={capitalise(g)}
+    />
+  ));
+  return (
+    <>
+      <label>What is your gender</label>
+      <div onChange={genderChangeHandler}>{genderRadios}</div>
+    </>
+  );
+};
+
+export default GenderPicker;
