67d6690ec3072551f0cae0e3a0b96c6cbea7ec64
joshuasearle <joshryansearle@gmail.com>
2020-12-29 19:47:04 +1100

    Add data manip func

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,4 @@
-import { time, gender } from './types';
+import { time, gender, bacData } from './types';
 
 export const capitalise = (s: string): string => {
   return s.charAt(0).toUpperCase() + s.slice(1);
@@ -40,5 +40,28 @@ export const widmark = (
 
   const alcoholMetabolised = 0.015 * timeElapsed;
   const totalBac = bacBeforeMetabolisation - alcoholMetabolised;
-  return totalBac;
+  return totalBac <= 0 ? 0 : totalBac;
+};
+
+export const createBacData = (bacs: number[], startTime: time): bacData => {
+  let currentTime = startTime;
+  const bacData: bacData = [];
+  for (let bac of bacs) {
+    bacData.push({ bac: bac, time: timeToString(currentTime) });
+    currentTime = nextTime(currentTime);
+  }
+  return bacData;
+};
+
+export const nextTime = ({ hours, minutes }: time): time => {
+  if (hours === 23 && minutes === 30) return { hours: 0, minutes: 0 };
+  if (minutes === 30) return { hours: hours + 1, minutes: 0 };
+  return { hours: hours, minutes: minutes + 30 };
+};
+
+export const timeToString = ({ hours, minutes }: time): string => {
+  const am = hours < 12 ? 'AM' : 'PM';
+  const hour = hours === 0 ? '12' : String(hours % 12);
+  const minute = String(minutes).length === 2 ? String(minutes) : `0${minutes}`;
+  return `${hour}:${minute}${am}`;
 };
